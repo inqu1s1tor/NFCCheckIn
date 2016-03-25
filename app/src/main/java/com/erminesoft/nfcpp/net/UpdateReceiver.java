545ebc2b959280812237fd38f536f7d48341904d
remi <Kbuba.v1.0@gmail.com>
2016-03-25 18:14:21 +0200

    reformat

--- app/src/main/java/com/erminesoft/nfcpp/net/UpdateReceiver.java
+++ app/src/main/java/com/erminesoft/nfcpp/net/UpdateReceiver.java
@@ -5,24 +5,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.erminesoft.nfcpp.util.NetworkUtil;
 
-/**
- * Created by Aleks on 25.03.2016.
- */
 public class UpdateReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        checkInetAndSync(context);
-    }
 
-    private void checkInetAndSync(Context context) {
-        String status = NetworkUtil.getConnectivityStatusString(context);
-        if(status == "Wifi enabled" || status == "Mobile data enabled") {
+        if (NetworkUtil.isConnected(context)) {
             SyncService.start(context);
         }
     }
