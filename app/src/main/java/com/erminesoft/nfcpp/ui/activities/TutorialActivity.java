1b3a00988fd266bdbc33d20930b1684e5f48f332
Aleksandr Nikitin <aleksnik.sn@gmail.com>
2016-04-06 13:26:57 +0300

    Tutorial

---
+++ app/src/main/java/com/erminesoft/nfcpp/ui/activities/TutorialActivity.java
@@ -0,0 +1,12 @@
+package com.erminesoft.nfcpp.ui.activities;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Aleks on 06.04.2016.
+ */
+public class TutorialActivity extends FragmentActivity {
+
+
+
+}
